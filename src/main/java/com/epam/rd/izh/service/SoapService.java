e6b221ccdc98f8a782f53e8bf86474fb1e593734
DubovtsevEgor <egor.dubovczcev@yandex.ru>
2021-03-01 10:51:53 +0400

    added rest call

--- src/main/java/com/epam/rd/izh/service/SoapService.java
+++ src/main/java/com/epam/rd/izh/service/SoapService.java
@@ -1,38 +1,50 @@
 package com.epam.rd.izh.service;
 
+import com.epam.rd.izh.dto.Todo;
 import com.epam.rd.izh.soap.com.dataaccess.webservicesserver.InvertStringCase;
 import com.epam.rd.izh.soap.com.dataaccess.webservicesserver.InvertStringCaseResponse;
-import com.epam.rd.izh.soap.com.dataaccess.webservicesserver.TextCasing;
-import com.epam.rd.izh.soap.com.dataaccess.webservicesserver.TextCasingSoapType;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.ws.client.core.WebServiceTemplate;
-import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
-import org.springframework.ws.soap.client.core.SoapActionCallback;
 
 public class SoapService {
+    private static final RestTemplate REST_TEMPLATE = new RestTemplate();
     public static void main(String[] args) {
-        TextCasing textCasing = new TextCasing();
+
         SoapService soapService = new SoapService();
         Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
         marshaller.setContextPaths("com.epam.rd.izh.soap.com.dataaccess.webservicesserver");
-        soapService.invertStringCase("lower casez");
+        System.out.println(soapService.invertStringCase("lower case", marshaller));
+
+        try {
+            System.out.println(soapService.getTodo());
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
 
 
-    public String invertStringCase(String sAString) {
+    public String invertStringCase(String lineForCaseInvert, Jaxb2Marshaller marshaller) {
 
         InvertStringCase request = new InvertStringCase();
-        request.setSAString(sAString);
-        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
-        marshaller.setContextPaths("com.epam.rd.izh.soap.com.dataaccess.webservicesserver");
+        request.setSAString(lineForCaseInvert);
+
         WebServiceTemplate webServiceTemplate = new WebServiceTemplate(marshaller);
 
 
         InvertStringCaseResponse response = (InvertStringCaseResponse) webServiceTemplate
-                .marshalSendAndReceive("https://www.dataaccess.com/webservicesserver/TextCasing.wso?op=InvertStringCase",
-                        request, new SoapActionCallback("https://www.dataaccess.com/webservicesserver/TextCasing.wso/InvertStringCase"));
-        System.out.println(response.getInvertStringCaseResult());
-        return null;
+                .marshalSendAndReceive("https://www.dataaccess.com/webservicesserver/TextCasing.wso?op=InvertStringCase", request);
+
+        return response.getInvertStringCaseResult();
     }
 
+    public Todo getTodo() throws JsonProcessingException {
+
+        String result = REST_TEMPLATE.getForObject("https://jsonplaceholder.typicode.com/todos/1", String.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        return objectMapper.readValue(result, Todo.class);
+    }
 }

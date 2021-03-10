669358bdf3785b6df597700d745d4c99bc4876b9
DubovtsevEgor <egor.dubovczcev@yandex.ru>
2021-03-10 15:12:41 +0400

    object mapper is outside

--- src/main/java/com/epam/rd/izh/service/SoapService.java
+++ src/main/java/com/epam/rd/izh/service/SoapService.java
@@ -5,12 +5,18 @@ import com.epam.rd.izh.soap.com.dataaccess.webservicesserver.InvertStringCase;
 import com.epam.rd.izh.soap.com.dataaccess.webservicesserver.InvertStringCaseResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.ws.client.core.WebServiceTemplate;
 
+@Service
 public class SoapService {
     private static final RestTemplate REST_TEMPLATE = new RestTemplate();
+    @Autowired
+    private ObjectMapper objectMapper;
+
     public static void main(String[] args) {
 
         SoapService soapService = new SoapService();
@@ -32,8 +38,6 @@ public class SoapService {
         request.setSAString(lineForCaseInvert);
 
         WebServiceTemplate webServiceTemplate = new WebServiceTemplate(marshaller);
-
-
         InvertStringCaseResponse response = (InvertStringCaseResponse) webServiceTemplate
                 .marshalSendAndReceive("https://www.dataaccess.com/webservicesserver/TextCasing.wso?op=InvertStringCase", request);
 
@@ -42,8 +46,8 @@ public class SoapService {
 
     public Todo getTodo() throws JsonProcessingException {
 
-        String result = REST_TEMPLATE.getForObject("https://jsonplaceholder.typicode.com/todos/1", String.class);
-        ObjectMapper objectMapper = new ObjectMapper();
+        String result = REST_TEMPLATE.getForObject("https://jsonplaceholder.typicode.com/todos/{todo}", String.class, "1");
+
 
         return objectMapper.readValue(result, Todo.class);
     }
